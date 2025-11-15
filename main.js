a1b3307318b2f77c976baba577b568770afc3372
Luke Pring <hello@lukepr.ing>
2025-11-15 20:27:28 +0000

    Updated Algothihm

--- main.js
+++ main.js
@@ -5,11 +5,18 @@
 
 
 function stackAlgorithm(stack) {
-    console.warn(stack);
+    console.warn(`Before Pop: ${stack}`);
+
+    // Pop two items at top of stack
     for (i = 0; i < 2; i++) {
         stack.pop()
-        console.info(stack);
+        console.info(`Pass ${i+1}: ${stack}`);
     }
+
+    // Compare and calculate two items at top
+    console.log(stack.length);
+
+    // Return modified stack
     return stack;
 } 
 
@@ -17,7 +24,7 @@ function main() {
 
     const exampleStack = [8,4,5,7,7,2,9,5,6];
 
-    console.log(stackAlgorithm(exampleStack));
+    console.log(`Final: ${stackAlgorithm(exampleStack)}`);
 
 }
 
