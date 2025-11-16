d1b0f5e5f7ccc01d2e2290833ac5d53d347025af
Luke Pring <hello@lukepr.ing>
2025-11-16 18:11:17 +0000

    Refactor stackAlgorithm: remove debug logs and update final output message

--- Task1.js
+++ Task1.js
@@ -3,17 +3,14 @@
 // University of Roehampton
 
 function stackAlgorithm(stack) {
-    console.warn(`Before Pop: ${stack}`);
     let l = stack[stack.length - 1];
     let s = stack[stack.length - 2];
-    while (stack.length > 1) {
 
+    while (stack.length > 1) {
         // Pop two items at top of stack
         for (i = 0; i < 2; i++) {
             stack.pop();
-            console.info(`Pass ${i + 1}: ${stack}`);
         }
-
         if (l % 2 == 0 && s % 2 == 0) {
             // Both Even
             stack.push(l * s);
@@ -30,10 +27,8 @@ function stackAlgorithm(stack) {
                 stack.push(0);
             }
         }
-
         l = stack[stack.length - 1];
         s = stack[stack.length - 2];
-
     }
 
     // Return modified stack
@@ -44,7 +39,7 @@ function main() {
 
     const exampleStack = [8, 4, 5, 7, 7, 2, 9, 5, 6];
 
-    console.log(`Final: ${stackAlgorithm(exampleStack)}`);
+    console.log(`Result: ${stackAlgorithm(exampleStack)}`);
 
 }
 
