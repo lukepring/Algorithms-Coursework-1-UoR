fc37570bf597d602feb0ff8f1c64753a0bd6226b
Luke Pring <hello@lukepr.ing>
2025-11-16 18:27:59 +0000

    Implement linked list structure with Node and Full_Name classes

--- Task2.js
+++ Task2.js
@@ -1,11 +1,36 @@
-const linkedList = {
-    head: {
-        value: 6,
-        next: {
-            value: 10,
-            next: {
-
-            }
-        }
+class Node {
+    constructor(position, letter, part) {
+        this.position = position;
+        this.letter = letter;
+        this.part = part;
+        this.next = null;
     }
-};
+}
+
+class Full_Name {
+    constructor() {
+        this.head = null;
+    }
+
+    Get_element(pos) {
+
+    }
+
+    Add_element(letter, part) {
+
+    }
+
+    Add_word(word, part) {
+
+    }
+
+    Search_element(letter) {
+
+    }
+
+    Print_list() {
+
+    }
+
+
+}
